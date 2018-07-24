946d114d32a7de8d3e8bc94440c3bc35188170c4
Evangelos Kalabokis <kalabokisevangelos@gmail.com>
2018-07-25 02:45:25 +0300

    Done with SW for now

--- js/main.js
+++ js/main.js
@@ -90,7 +90,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: 'pk.eyJ1IjoiZXZhbmdlbG9za2FsIiwiYSI6ImNqanp6dzNyZTAxbWYzcGtlbDBtaHVidGYifQ.jaLXUI3lr9TfH_r6Vfu6oQ',
+    mapboxToken: '<your MAPBOX API KEY HERE>',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
--- sw.js
+++ sw.js
@@ -1,20 +1,22 @@
+let staticCacheName = "restaurantCache3";
+
 self.addEventListener('install', function(event) {
 
   event.waitUntil(
-    caches.open('restaurantCache').then(function(cache) {
+    caches.open(staticCacheName).then(function(cache) {
       return cache.addAll([
         '/',
         '/restaurant.html',
-        '/img/1.jpg',
-        '/img/2.jpg',
-        '/img/3.jpg',
-        '/img/10.jpg',
-        '/img/4.jpg',
-        '/img/5.jpg',
-        '/img/6.jpg',
-        '/img/7.jpg',
-        '/img/8.jpg',
-        '/img/9.jpg',
+        // '/img/1.jpg',
+        // '/img/2.jpg',
+        // '/img/3.jpg',
+        // '/img/10.jpg',
+        // '/img/4.jpg',
+        // '/img/5.jpg',
+        // '/img/6.jpg',
+        // '/img/7.jpg',
+        // '/img/8.jpg',
+        // '/img/9.jpg',
         '/css/responsive_index.css',
         '/css/responsive_restaurant.css',
         '/css/styles.css',
@@ -22,9 +24,10 @@ self.addEventListener('install', function(event) {
         '/js/dbhelper.js',
         '/js/main.js',
         '/js/restaurant_info.js',
-        // 'https://api.tiles.mapbox.com/v4/',
+        'https://api.mapbox.com/mapbox-gl-js/v0.47.0/mapbox-gl.js',
+        'https://api.mapbox.com/mapbox-gl-js/v0.47.0/mapbox-gl.css',
+        'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
         'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
-        'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css'
       ]);
     })
   );
@@ -34,18 +37,44 @@ self.addEventListener('fetch', function(event) {
 
   event.respondWith(
     caches.match(event.request).then(function(response) {
-      if (response) return response;
-      return fetch(event.request);
+      if (response) {
+        return response;
       }
-    )
-  )
+    return fetch(event.request);
+
+// The code following caches EVERYTHING
+    //
+    //   var requestClone = event.request.clone();
+    //
+    //   return fetch(requestClone).then(function(response) {
+    //     if(!response) {
+    //         console.log('[ServiceWorker] No response from fetch');
+    //         return response;
+    //     }
+    //
+    //     var responseClone = response.clone();
+    //                   // Open the cache again
+    //     return caches.open('staticCacheName').then(function(cache) {
+    //       cache.put(event.request, responseClone);
+    //       return response;
+    //     })
+    //
+    //   }
+    // )
+  }))
 });
 
-// let requestUrl = new URL(event.request.url);
-//
-// if (requestUrl.origin === location.origin) {
-//   if (requestUrl.pathname === '/') {
-//     event.respondWith(caches.match('/skeleton'));
-//     return;
-//   }
-// }
+// Clearing old cache
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+        return cacheName.startsWith('restaurant') && cacheName != staticCacheName;
+      }).map(function(cacheName) {
+        return cache.delete(cacheName);
+      })
+    );
+    })
+  );
+});
