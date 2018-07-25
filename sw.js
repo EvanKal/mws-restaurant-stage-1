8f08c04d92a9e5cbeb38ff3d1d0b3537b601d4fe
Evangelos Kalabokis <kalabokisevangelos@gmail.com>
2018-07-25 05:28:26 +0300

    Cached pages visited!

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-let staticCacheName = "restaurantCache4";
+let staticCacheName = "restaurantCache10";
 
 self.addEventListener('install', function(event) {
 
@@ -6,41 +6,51 @@ self.addEventListener('install', function(event) {
     caches.open(staticCacheName).then(function(cache) {
       return cache.addAll([
         '/',
-        '/restaurant.html ',
-        // '/img/1.jpg',
-        // '/img/2.jpg',
-        // '/img/3.jpg',
-        // '/img/10.jpg',
-        // '/img/4.jpg',
-        // '/img/5.jpg',
-        // '/img/6.jpg',
-        // '/img/7.jpg',
-        // '/img/8.jpg',
-        // '/img/9.jpg',
+        '/restaurant.html',
         '/css/responsive_index.css',
         '/css/responsive_restaurant.css',
         '/css/styles.css',
-        '/data/restaurants.json',
         '/js/dbhelper.js',
         '/js/main.js',
-        '/js/restaurant_info.js',
-        'https://api.mapbox.com/mapbox-gl-js/v0.47.0/mapbox-gl.js',
-        'https://api.mapbox.com/mapbox-gl-js/v0.47.0/mapbox-gl.css',
-        'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
-        'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
+        '/js/restaurant_info.js'
       ]);
     })
   );
 });
 
+//In the next listener the restaurant pages that have been visited by the user
+// are being additionaly cached
+
 self.addEventListener('fetch', function(event) {
 
   event.respondWith(
+// First check for a match of the request in the cache
     caches.match(event.request).then(function(response) {
+// if there is a match then respond with matched request from cache
       if (response) {
         return response;
       }
+// if there is NOT a match then fetch it from the network, but before that
+// check if it is a request for a restaurant page and if yes cache its html
+    if (!event.request.url.includes('restaurant.html?id=')) {
     return fetch(event.request);
+  } else {
+      let requestClone = event.request.clone();
+
+      return fetch(requestClone).then(function(response) {
+        if(!response) {
+            return response;
+        }
+
+        let responseClone = response.clone();
+        return caches.open(staticCacheName).then(function(cache) {
+          cache.put(event.request, responseClone);
+          return response;
+        })
+
+      }
+    )
+  }
 
 // The code following caches EVERYTHING
     //
@@ -64,7 +74,7 @@ self.addEventListener('fetch', function(event) {
   }))
 });
 
-// Clearing old cache
+// Clear old cache
 self.addEventListener('activate', function(event) {
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
