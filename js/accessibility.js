aecd2e06dd50f84d07f8c2214a28d344c1674f7a
Evangelos Kalabokis <kalabokisevangelos@gmail.com>
2018-07-27 20:12:10 +0300

    Commit before creating alt text for images

--- js/accessibility.js
+++ js/accessibility.js
@@ -28,12 +28,73 @@ function headerAccessibility() {
 
 function mainAccessibility() {
 // Map
-  $("#map-container").after("<label id=\"mapLabel\">Map of New York restaurants with a review</label>");
-  $("#map-container").attr("aria-labelledby", "mapLabel");
-  $("#map-container").attr("role", "application");
+  let mapContainer = $("#map-container");
+  let map = $("#map");
+
+  mapContainer.after("<label id=\"mapLabel\" style=\"display: none\">Map of New York restaurants with a review</label>");
+  mapContainer.attr("aria-labelledby", "mapLabel");
+  mapContainer.attr("role", "application");
   // $("#map").attr("aria-hidden", "true");
-  $("#map").find("img").attr("tabindex", "-1").attr("aria-hidden", "true");
-  $("#map").find("a").attr("tabindex", "-1").attr("aria-hidden", "true");
-  $("#map").find("div").attr("tabindex", "-1").attr("aria-hidden", "true");
+  map.find("img").attr("tabindex", "-1").attr("aria-hidden", "true");
+  map.find("a").attr("tabindex", "-1").attr("aria-hidden", "true");
+  map.find("div").attr("tabindex", "-1").attr("aria-hidden", "true");
+
+// Filter results
+
+  let allNeighbourhoods = $("#neighborhoods-select");
+  let allCousines = $("#cuisines-select");
+
+  $(".filter-options").find("h2").after("<label id=\"filterResults\" style=\"display: none\">Filter options</label>");
+  allNeighbourhoods.attr("aria-labelledby", "filterResults").attr("role", "combobox");
+  allNeighbourhoods.attr("aria-owns", "restaurants-list");
+  allCousines.attr("aria-labelledby", "filterResults");
+  allCousines.attr("aria-controls", "restaurants-list");
+
+  let restautantsList = $('#restaurants-list');
+
+  restautantsList.attr("aria-live", "polite");
+  restautantsList.attr("aria-atomic", "true");
+
+// Restaurants in restaurantsList
+
+  let restautantsInList;
+  restautantsInList = $('#restaurants-list').find("li");
+  let arrayFromList = restautantsInList.toArray();
+  console.log(arrayFromList);
+
+  arrayFromList.forEach(function(elem) {
+  $(elem).attr("tabindex", "0").attr("aria-labelledby", `nameOfLi${arrayFromList.indexOf(elem)}`);
+  $(elem).find("img").attr("tabindex", "0");
+  $(elem).find("p").attr("tabindex", "0");
+  $(elem).find("a").attr("tabindex", "0");
+  $(elem).find("h1").attr("id", `nameOfLi${arrayFromList.indexOf(elem)}`);
+  })
+
+
+
+
+  restautantsList.on("DOMSubtreeModified", function() {
+    restautantsInList = $('#restaurants-list').find("li");
+    restautantsInList = $('#restaurants-list').find("li");
+    let arrayFromList = restautantsInList.toArray();
+    console.log(arrayFromList);
+
+    arrayFromList.forEach(function(elem) {
+    $(elem).attr("tabindex", "0").attr("aria-labelledby", `nameOfLi${arrayFromList.indexOf(elem)}`);
+    $(elem).find("img").attr("tabindex", "0").attr("alt");
+    $(elem).find("p").attr("tabindex", "0");
+    $(elem).find("a").attr("tabindex", "0");
+    $(elem).find("h1").attr("id", `nameOfLi${arrayFromList.indexOf(elem)}`);
+    })
+
+
+  })
 
 }
+
+// function restaurantsAccessibility() {
+//
+//
+//
+//
+// }
